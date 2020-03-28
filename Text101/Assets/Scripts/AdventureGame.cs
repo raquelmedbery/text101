a3bd93e839e4a4c96f293e7e81c3149dc4947480
raquelmedbery <50762368+raquelmedbery@users.noreply.github.com>
2020-03-28 03:26:27 -0500

    adding final project

---
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //need to use this to access Text type
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent; //this means this is available in our Inspector
+    [SerializeField] State startingState;
+
+    
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory(); //access the text property found in the text component
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+        }
+
+        textComponent.text = state.GetStateStory();
+    }
+}
